5c83eb535c40ed2c075c3a2a912b0d3f07d4ffc3
Dipak Garai <garaidipak1998@gmail.com>
2021-10-17 19:57:14 +0530

    Final Todo App

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,24 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import todo from '../assets/images/todo.png'
+
+// get data form local storge
+const getLocalData = () => {
+  let list = localStorage.getItem('todo-list')
+
+  if (list) {
+    return JSON.parse(list)
+  } else {
+    return []
+  }
+}
 
 const Todo = () => {
   const [inputData, setInputData] = useState('')
-  const [todoList, setTodoList] = useState([])
+  const [todoList, setTodoList] = useState(getLocalData())
+  const [toggleBtn, setToggleBtn] = useState(false)
+  const [isEditItem, setIsEditItem] = useState()
   //   to add todos in the list
   const addItem = () => {
-    if (inputData) {
-      setTodoList([...todoList, inputData])
-      setInputData('')
-    } else {
+    if (!inputData) {
       alert('Please Write something inside input field....')
+    } else if (inputData && toggleBtn) {
+      setTodoList(
+        todoList.map((elem) => {
+          if (elem.id === isEditItem) {
+            return { ...elem, todo: inputData }
+          }
+          return elem
+        })
+      )
+      setToggleBtn(true)
+      setInputData('')
+      setIsEditItem(null)
+    } else {
+      const allInputData = {
+        id: new Date().getTime().toString(),
+        todo: inputData,
+      }
+      setTodoList([...todoList, allInputData])
+      setInputData('')
     }
+  }
+  // edit todo
+  const editItem = (id) => {
+    let editItem = todoList.find((elem) => {
+      return elem.id === id
+    })
+    console.log(editItem)
+    setToggleBtn(true)
+    setInputData(editItem.todo)
+    setIsEditItem(id)
   }
   //   delete single todo
   const deleteItem = (id) => {
-    console.log(id)
-    const updateTodoList = todoList.filter((todo, idx) => {
+    const updateTodoList = todoList.filter((todo) => {
       // [html = > 0]
-      return idx !== id
+      return id !== todo.id
     })
     setTodoList(updateTodoList)
   }
@@ -26,6 +64,12 @@ const Todo = () => {
   const removeAll = () => {
     setTodoList([])
   }
+
+  // add items/todos inside local storage
+
+  useEffect(() => {
+    localStorage.setItem('todo-list', JSON.stringify(todoList))
+  }, [todoList])
   return (
     <div className='main-div'>
       <div className='child-div'>
@@ -42,18 +86,33 @@ const Todo = () => {
               setInputData(e.target.value)
             }}
           />
-          <i className='fas fa-plus' title='add item' onClick={addItem}></i>
+          {toggleBtn ? (
+            <i
+              className='fas fa-edit'
+              title='Update item'
+              onClick={addItem}
+            ></i>
+          ) : (
+            <i className='fas fa-plus' title='add item' onClick={addItem}></i>
+          )}
         </div>
         <div className='showItems'>
-          {todoList.map((todo, id) => {
+          {todoList.map((todo) => {
             return (
-              <div className='eachItem' key={id}>
-                <h3>{todo}</h3>
-                <i
-                  className='fas fa-trash-alt add-btn'
-                  title='delete item'
-                  onClick={() => deleteItem(id)}
-                ></i>
+              <div className='eachItem' key={todo.id}>
+                <h3>{todo.todo}</h3>
+                <div className='todo-btn'>
+                  <i
+                    className='fas fa-edit add-btn'
+                    title='Edit item'
+                    onClick={() => editItem(todo.id)}
+                  ></i>
+                  <i
+                    className='fas fa-trash-alt add-btn'
+                    title='delete item'
+                    onClick={() => deleteItem(todo.id)}
+                  ></i>
+                </div>
               </div>
             )
           })}
